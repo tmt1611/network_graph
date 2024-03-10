38f77dcca3cfffd8caabbaa6e09c9e10ed352719
tmt1611 <82852830+tmt1611@users.noreply.github.com>
2024-03-10 16:26:15 +0100

    Update main.py

--- tmt1611.github.io/main.py
+++ tmt1611.github.io/main.py
@@ -6,6 +6,7 @@ from community import community_louvain
 import numpy as np
 from alphashape import alphashape
 import datetime
+import panda as pd
 
 def alpha_shape(coordinates, alpha=0):
     # Compute the alpha-shape
